142010dbe301ad890efd49505fbb525d11a1f093
tongzhipeng <tongzhipeng5688@gmail.com>
2014-11-15 19:37:06 +0800

    add non-member operator demo

---
+++ DemoCodes/non-member-operator/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Rational
+{
+public:
+	Rational(int a = 0) :m_a(a)
+	{
+	}
+
+	~Rational()
+	{
+	}
+
+	void print()
+	{
+		cout << m_a << endl;
+	}
+private:
+	int m_a;
+
+	friend const Rational operator* (const Rational& lhs, const Rational& rhs);
+};
+
+const Rational operator* (const Rational& lhs, const Rational& rhs)
+{
+	Rational ret;
+	ret.m_a =  lhs.m_a * rhs.m_a;
+	return ret;
+}
+
+int main()
+{
+	Rational a(3), b(2);
+	Rational c = a*b;
+	c.print();
+
+	system("pause");
+	return 0;
+}
